504eb145acce96d36dada2a689034259e1d0865e
Shyamala131293 <shyamala.devi1993@gmail.com>
2026-08-02 17:37:40 +0530

    Change variable 'port' to 'PORT' for consistency

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-var port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
