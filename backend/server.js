f343335b9793ade9ed73ff7f683cf2f5defa846d
Shyamala131293 <shyamala.devi1993@gmail.com>
2026-08-03 05:40:10 +0530

    Change SendGrid API key in server.js
    Updated SendGrid API key for email functionality.

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 // Set your SendGrid API Key
-sgMail.setApiKey('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');
+sgMail.setApiKey('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');
 
 app.post('/send-email', async (req, res) => {
   const { recipientEmails, pdfBase64 } = req.body;
