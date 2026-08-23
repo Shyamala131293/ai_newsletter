f44448492e2483c3e91cb82e4b2dbbfe01455fb9
Shyamala131293 <shyamala.devi1993@gmail.com>
2026-08-23 23:16:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function fetchArticles(domain) {
   const url = `${domain}`; // Fixed: Use backticks for template literal
-   const testUrl = 'https://en.wikipedia.org/wiki/Main_Page';
+   const testUrl = 'https://dzone.com/';
   console.log(testUrl);
   const { data } = await axios.get(testUrl, {
   headers: {
